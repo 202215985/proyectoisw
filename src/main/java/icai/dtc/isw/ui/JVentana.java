98f712218c717f2d41d89c653af34ba9754cba1a
nacho <202200959@alu.comillas.edu>
2025-11-03 18:23:45 +0100

    prueba

--- src/main/java/icai/dtc/isw/ui/JVentana.java
+++ src/main/java/icai/dtc/isw/ui/JVentana.java
@@ -1,13 +1,27 @@
 
-
+//Prueba
 package icai.dtc.isw.ui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.util.HashMap;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 import icai.dtc.isw.client.Client;
 import icai.dtc.isw.domain.Customer;
-
-import javax.swing.*;
-import java.awt.*;
-import java.util.HashMap;
 
 public class JVentana extends JFrame {
     public static void main(String[] args) {
